e29063f454e9cf56bd3042a4d72d9fd6c8f3957b
yuquan <yuquan@wacai.com>
2018-10-10 14:31:26 +0800

    完成所有弹窗相关的Demo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -56,6 +56,10 @@ export default class HomeScreen extends React.Component {
       modalAlignCenterVisible: false,
       modalAlignTopVisible: false,
       modalAlignEndVisible: false,
+
+      dialogVisible: false,
+
+      popupVisible: false
     }
   }
 
@@ -64,7 +68,7 @@ export default class HomeScreen extends React.Component {
       <ScrollView
         style={styles.container}
         contentContainerStyle={styles.container}>
-        <ContentItem title='1. Modal弹窗'>
+        <ContentItem title='1. Modal基本弹窗'>
           <Button text='居中显示' onPress={this.showModalAlignCenter} />
           <Modal 
             visible={this.state.modalAlignCenterVisible}
@@ -105,8 +109,100 @@ export default class HomeScreen extends React.Component {
         </ContentItem>
 
 
+        <ContentItem title='2. Dialog确认弹窗'>
+          <Button text='常用弹窗' onPress={this.showDialog}/>
+          <Dialog
+            title='开启通讯录访问权限'
+            visible={this.state.dialogVisible}
+            cancelButton={{
+              onPress: this.hideDialog
+            }}
+            confirmButton={{
+              onPress: this.hideDialog
+            }}>
+            <Text>为了更好的为您服务，我们需要您开启应用的通讯录访问权限</Text>
+          </Dialog>
+        </ContentItem>
 
+        <ContentItem title='3. Popup底部弹窗'>
+          <Button text='底部弹窗' onPress={this.showPopup}/>
+          <Popup
+            title='请设置标题'
+            visible={this.state.popupVisible}
+            onClose={this.hidePopup}
+            cancelButton={{
+              onPress: this.hidePopup
+            }}
+            confirmButton={{
+              onPress: this.hidePopup
+            }}>
+            <View style={{paddingVertical: 42, alignItems: 'center'}}>
+              <Text>这里设置底部弹窗的显示内容</Text>
+            </View>
+          </Popup>
+        </ContentItem>
 
+        <ContentItem title='4. Popover悬浮弹窗'>
+          <Popover
+            position='right'
+            itemList={[{
+              name: '扫一扫'
+            }, {
+              name: '帮助'
+            }]}
+            renderItem={this.renderPopoverItem}
+            contentContainerStyle={{padding: 6}}
+            onPress={this.onPopoverItemPress}>
+            <View style={[styles.button, styles.buttonNoMargin]}>
+              <Text style={styles.buttonText}>Right</Text>
+            </View>    
+          </Popover>
+
+          <Popover
+            position='top'
+            itemList={[{
+              name: '扫一扫'
+            }, {
+              name: '帮助'
+            }]}
+            renderItem={this.renderPopoverItem}
+            contentContainerStyle={{padding: 6}}
+            onPress={this.onPopoverItemPress}>
+            <View style={[styles.button, styles.buttonNoMargin]}>
+              <Text style={styles.buttonText}>Top</Text>
+            </View>    
+          </Popover>
+
+          <Popover
+            position='bottom'
+            itemList={[{
+              name: '扫一扫'
+            }, {
+              name: '帮助'
+            }]}
+            renderItem={this.renderPopoverItem}
+            contentContainerStyle={{padding: 6}}
+            onPress={this.onPopoverItemPress}>
+            <View style={[styles.button, styles.buttonNoMargin]}>
+              <Text style={styles.buttonText}>Bottom</Text>
+            </View>    
+          </Popover>
+
+          <Popover
+            position='left'
+            itemList={[{
+              name: '扫一扫'
+            }, {
+              name: '帮助'
+            }]}
+            renderItem={this.renderPopoverItem}
+            contentContainerStyle={{padding: 6}}
+            onPress={this.onPopoverItemPress}>
+            <View style={[styles.button, styles.buttonNoMargin]}>
+              <Text style={styles.buttonText}>Left</Text>
+            </View>
+          </Popover>
+        </ContentItem>
       </ScrollView>
     )
   }
@@ -146,6 +242,52 @@ export default class HomeScreen extends React.Component {
       modalAlignEndVisible: false
     })
   }
+
+  showDialog = () => {
+    this.setState({
+      dialogVisible: true    
+    })
+  }
+
+  hideDialog = () => {
+    this.setState({
+      dialogVisible: false
+    })
+  }
+
+  showPopup = () => {
+    this.setState({
+      popupVisible: true    
+    })
+  }
+
+  hidePopup = () => {
+    this.setState({
+      popupVisible: false
+    })
+  }
+
+  showPopoverBottom = () => {
+    this.setState({
+      popoverBottomVisible: true    
+    })
+  }
+
+  hidePopoverBottom = () => {
+    this.setState({
+      popoverBottomVisible: false
+    })
+  }
+
+  renderPopoverItem = (item) => {
+    return (
+      <Text>{item.name}</Text>    
+    )
+  }
+
+  onPopoverItemPress = (item) => {
+    console.warn('您点击了：' + item.name)
+  }
 }
 
 const styles = StyleSheet.create({
@@ -176,6 +318,10 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+
+  buttonNoMargin: {
+    margin: 0                
   },
 
   buttonText: {
